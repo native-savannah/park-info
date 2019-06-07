3a0ab6de95d928c405452e5550db4aa46bdebdda
Bryan Fordham <bfordham@gmail.com>
2019-06-07 00:21:11 -0400

    Add the other parks in at Disney World

--- lib/disney/disney_world.rb
+++ lib/disney/disney_world.rb
@@ -5,5 +5,23 @@ module ParkInfo
             PARK_ID = "80007823"
             REGION = 'us'
         end
+
+        class Epcot < DisneyResort
+            RESORT_ID = "80007798";
+            PARK_ID = "80007838";
+            REGION = "us";
+        end
+
+        class HollywoodStudios < DisneyResort
+            RESORT_ID = "80007798";
+            PARK_ID = "80007998";
+            REGION = "us";
+        end
+
+        class MagicKingdom < DisneyResort
+            RESORT_ID = "80007798";
+            PARK_ID = "80007944";
+            REGION = "us";
+        end
     end
 end
